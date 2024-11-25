78780308b6c898f4a3be1c3a3f5267eb37c7de82
qinluo <eric.x.sun@gmail.com>
2024-11-25 17:37:34 +0800

    feat: shuf while build index

--- sbin/check.sh
+++ sbin/check.sh
@@ -9,9 +9,9 @@ PROJECT_ROOT=${SELF_DIR}/..
 check_fmt() {
   cd ${PROJECT_ROOT}
 
-  ${ROOT_PYTHON_BIN:+${ROOT_PYTHON_BIN}/}isort hakkero || exit 1
-  ${ROOT_PYTHON_BIN:+${ROOT_PYTHON_BIN}/}autopep8 hakkero || exit 1
-  ${ROOT_PYTHON_BIN:+${ROOT_PYTHON_BIN}/}black hakkero || exit 1
+  ${ROOT_PYTHON_BIN:+${ROOT_PYTHON_BIN}/}isort hakkero setup.py || exit 1
+  ${ROOT_PYTHON_BIN:+${ROOT_PYTHON_BIN}/}autopep8 hakkero setup.py || exit 1
+  ${ROOT_PYTHON_BIN:+${ROOT_PYTHON_BIN}/}black hakkero setup.py || exit 1
 
   cd ${SELF_DIR}
 }
